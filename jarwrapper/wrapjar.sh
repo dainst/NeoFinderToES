7c9b0f9ea2c22901b51a85e3b3e3debd224cc6cd
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-03-01 14:28:54 +0100

    change output binary extension to sh

--- jarwrapper/wrapjar.sh
+++ jarwrapper/wrapjar.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Wrapping jar file..."
-cat jarwrapper/base.sh target/NeoFinderToES-1.0-SNAPSHOT-jar-with-dependencies.jar > target/neofindertoes && chmod +x target/neofindertoes
+cat jarwrapper/base.sh target/NeoFinderToES-1.0-SNAPSHOT-jar-with-dependencies.jar > target/neofindertoes.sh && chmod +x target/neofindertoes.sh
